ee4f964b92e89bbda43a1cb38ecec60547fd4ec6
Methujan Jeyapalan <methujan_02@hotmail.com>
2021-05-24 18:18:00 +0000

    "Head"

---
+++ head.js
@@ -0,0 +1,23 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} ===  ${expected}`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const head = function(array) {
+  if (array === null) {
+    return undefined;
+
+  } else {
+    console.log(array[0]);
+    return array[0];
+  }
+};
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+
