e209a4040a47694fba2c074d1620003326e53864
Methujan Jeyapalan <methujan_02@hotmail.com>
2021-05-27 19:49:31 +0000

    Returns a slice of the array until truthy value from callback

---
+++ takeUntil.js
@@ -0,0 +1,16 @@
+const takeUntil = function(array, callback) {
+  results = [];
+  for(const element of array){
+    if(callback(element)){
+      return results;
+    } else{
+      results.push(element)
+    }
+  }
+  return results;
+}
+
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+console.log(results1);
