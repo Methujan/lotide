c662f1000c9d1be6a97622ce3ea805fdf912f78c
Methujan Jeyapalan <methujan_02@hotmail.com>
2021-05-28 03:30:52 +0000

    Simplifed code

--- without.js
+++ without.js
@@ -32,16 +32,17 @@ const assertArraysEqual = function(array1, array2) {
 };
 
 const without = function(source, itemsToRemove){
-  let newArray = [...source];
-  for (let i = 0; i <= source.length; i++) {
-    for (let x of itemsToRemove){
-      if(source[i] === x){
-        let removedItem = newArray.indexOf(x);
-        newArray.splice(removedItem,1);
+  let newArray = source; // Declaring a variable for the source array
+  for (let i = 0; i <= source.length; i++) { // Going through elements for the length of source
+    for (let x of itemsToRemove){ // Going through the elements in itemsToRemove
+      if(source[i] === x){ // If an element in source and itemsToRemove have the same equal value
+        let removedItem = newArray.indexOf(x); // let removedItem be the index of x in the newArray
+        console.log(removedItem);
+        newArray.splice(removedItem,1); // remove the value at index x 
       }
     }
   } 
   return newArray;
 }
 
-console.log(without([1,2,3,4,5,6,7,8],[8]));
+console.log(without([1,2,3,4,5,6,7,8],[6]));
