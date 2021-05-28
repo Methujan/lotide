e1d9a01784ecdd932566dd70063833c94bd1a2c7
Methujan Jeyapalan <methujan_02@hotmail.com>
2021-05-28 01:27:23 +0000

    Added comments

--- head.js
+++ head.js
@@ -8,16 +8,14 @@ const assertEqual = function(actual, expected) {
 
 const head = function(array) {
   if (array === null) {
-    return undefined;
+    return undefined; // If the array is null, then return undefined
 
   } else {
-    console.log(array[0]);
-    return array[0];
+    console.log(array[0]); // Print the first element in the array
+    return array[0]; // Returnt the first element in the array
   }
 };
 
+
 assertEqual(head([5,6,7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-
