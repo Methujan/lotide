06ad67cd9603d14bf65bbee286f0f5986ec78901
Methujan Jeyapalan <methujan_02@hotmail.com>
2021-05-28 02:07:55 +0000

    Added comments

--- tail.js
+++ tail.js
@@ -11,7 +11,12 @@ const tail = function(array) {
     return undefined;
 
   } else {
-    let tailedArray = array.slice(1);
+    let tailedArray = array.slice(1);// returns a portion of the array starting from the index in the brackets to the end
     return tailedArray;
   }
 };
+
+
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+console.log(tail(words)); // Prints the everything except the first element: Prints ["Lighthouse", "Labs"]
+assertEqual(words.length, 3); // original array should still have 3 elements!
