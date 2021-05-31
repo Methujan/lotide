046d8a4decae4f2afe51cf28d799489c4e6732a2
Methujan Jeyapalan <methujan_02@hotmail.com>
2021-05-31 20:24:36 +0000

    Refactored map

--- map.js
+++ map.js
@@ -1,18 +1,17 @@
 const words = ["ground", "control", "to", "major", "tom"];
 
 const map = function(array, callback) {
-  //console.log('array: ', array);
-  //console.log('callback: ', callback);
+
   const results = [];
 
   for (let item of array) {
     results.push(callback(item));
-    //console.log('item BEFORE: ', item);
-    //console.log('item AFTER: ', callback(item));
-    //console.log('---');
+ 
   }
   return results;
 }
+
+module.exports = map;
 
 const results1 = map(words, word => word[0]);
 console.log(results1);
