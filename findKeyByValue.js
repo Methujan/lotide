4206a9c0190ffedcd16e67f68c75a478171acf0d
Methujan Jeyapalan <methujan_02@hotmail.com>
2021-05-31 19:36:25 +0000

    Refactored findKeyByValue

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,26 +1,22 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} ===  ${expected}`);
-  } else {
-    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
 
 const findKeyByValue = function(objects, searchKey){
+  console.log(Object.keys(objects))
 for(const key of Object.keys(objects)){
+
   if(objects[key] === searchKey){
     return key;
   }
  }
 };
 
+module.exports = findKeyByValue;
 
 //TEST CODE
-const bestTVShowsByGenre = { 
-  sci_fi: "The Expanse",
-  comedy: "Brooklyn Nine-Nine",
-  drama:  "The Wire"
-};
+//const bestTVShowsByGenre = { 
+  //sci_fi: "The Expanse",
+  //comedy: "Brooklyn Nine-Nine",
+  //drama:  "The Wire"
+//};
 
-assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
-assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+//assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+//assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
