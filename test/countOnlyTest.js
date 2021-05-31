5e94164ae47b077565d7dbbd6fee8aef8f3afe01
Methujan Jeyapalan <methujan_02@hotmail.com>
2021-05-31 19:23:59 +0000

    test for countOnly

---
+++ test/countOnlyTest.js
@@ -0,0 +1,34 @@
+const countOnly = require('../countOnly')
+const assert = require('chai').assert;
+
+describe("#countLetters", () => {
+
+  const firstNames = [
+    "Karl",
+    "Salima",
+    "Agouhanna",
+    "Fang",
+    "Kavith",
+    "Jason",
+    "Salima",
+    "Fang",
+    "Joe"
+  ];
+
+  const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+  
+  it("returns 1 for result1[\'Jason\']", () => {
+    assert.strictEqual(result1["Jason"], 1);
+  });
+  
+  
+  it("returns undefined for result1[\'Karima\']", () => {
+    assert.strictEqual(result1["Karima"], undefined);
+  });
+  
+  
+  it("returns 2 for result1[\'Fang\']", () => {
+    assert.strictEqual(result1["Fang"], 2);
+  });
+  
+});
