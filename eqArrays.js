f3ee46c6089cad16ceeae58950916d7d4b36a076
Methujan Jeyapalan <methujan_02@hotmail.com>
2021-05-31 17:08:01 +0000

    Refactored eqArrays

--- eqArrays.js
+++ eqArrays.js
@@ -22,19 +22,5 @@ const eqArrays = function(array1, array2) {
   return result;
 };
 
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} ===  ${expected}`);
-  } else {
-    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
 
-
-assertEqual((eqArrays([1, 2, 3], [1, 2, 3])), true) // => true
-assertEqual((eqArrays([1, 2, 3], [3, 2, 1])), true) // => true
-
-assertEqual((eqArrays(["1", "2", "3"], ["1", "2", "3"])), true) // => true
-assertEqual((eqArrays(["1", "2", "3"], ["1", "2", 3])), true) // => true
-
-
+module.exports = eqArrays;
