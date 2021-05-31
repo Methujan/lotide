0ea46985119dde5a16c3dcb3a3f89e49ac16cccb
Methujan Jeyapalan <methujan_02@hotmail.com>
2021-05-31 18:58:08 +0000

    Refactored

--- eqObjects.js
+++ eqObjects.js
@@ -1,34 +1,6 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} ===  ${expected}`);
-  } else {
-    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
 
-const eqArrays = function(array1, array2) {
-  let result = true;
-  
-  if (array1.length === array2.length) {
+const eqArrays = require('./eqArrays')
 
-    for (let i = 0; i < array1.length; i++) {
-      
-      if (array1[i] === array2[i]) {
-        result = true;
-
-      } else {
-        result = false;
-        return result;
-
-      }
-    }
-  } else {
-    result = false;
-    return result;
-  }
-
-  return result;
-};
 // Returns true if both objects have identical keys with identical values.
 // Otherwise you get back a big fat false!
 const eqObjects = function(object1, object2) {
@@ -56,17 +28,5 @@ const eqObjects = function(object1, object2) {
 } return true
 };
 
-//TEST CODE
-const ab = { a: "1", b: "2" };
-const ba = { b: "2", a: "1" };
-assertEqual(eqObjects(ab, ba), true);
+module.exports = eqObjects;
 
-const abc = { a: "1", b: "2", c: "3" };
-assertEqual(eqObjects(ab, abc), false);
-
-const cd = { c: "1", d: ["2", 3] };
-const dc = { d: ["2", 3], c: "1" };
-assertEqual(eqObjects(cd, dc), true);
-
-const cd2 = { c: "1", d: ["2", 3, 4] };
-assertEqual(eqObjects(cd, cd2), false);
