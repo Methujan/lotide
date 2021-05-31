63d0017004fd81232cc505d35d8011f48c74a688
Methujan Jeyapalan <methujan_02@hotmail.com>
2021-05-31 18:19:10 +0000

    tests for lotide functions

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,10 +1,29 @@
 const middle = require('../middle')
-const eqArrays = require('../eqArrays');
-const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
 
 
+describe("#middle", () => {
+  it("returns [] for [1,2]", () => {
+    assert.deepEqual(middle([1,2]),[]);
+  });
+
+  it("returns [2] for [1,2,3]", () => {
+    assert.deepEqual(middle([1,2,3]),[2]);
+  });
+
+  it("returns [2,3] for [1,2,3,4]", () => {
+    assert.deepEqual(middle([1,2,3,4]),[2,3]);
+  });
+
+
+});
+
+
+/*
 assertArraysEqual(middle([1,2]),[]);
 assertArraysEqual(middle([1,2,3]),[2]);
 assertArraysEqual(middle([1,2,3,4]),[2,3]);
 assertArraysEqual(middle([1,2,3,4,5]),[3]);
 assertArraysEqual(middle([1,2,3,4,5,6,7,8,9,10]), [5,6]);
+*/
+
