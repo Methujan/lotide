cfb3025152d9a8ec4d525f8068d5b9b570516ff2
Methujan Jeyapalan <methujan_02@hotmail.com>
2021-05-31 18:39:12 +0000

    Exporting all functions

--- index.js
+++ index.js
@@ -18,21 +18,21 @@ const without = require('./without');
 
 
 module.exports = {
-  head:   head,
-  tail:   tail,
-  middle: middle,
-  assertArraysEqual: assertArraysEqual,
-  assertEqual: assertEqual,
-  assertObjectsEqual: assertObjectsEqual,
-  countLetters: countLetters,
-  eqArrays: eqArrays,
-  countOnly: countOnly ,
-  eqObjects: eqObjects,
-  findKey: findKey,
-  findKeyByValue: findKeyByValue,
-  letterPositions: letterPositions,
-  map: map,
-  takeUntil: takeUntil,
-  without: without
+  head,
+  tail,
+  middle,
+  assertArraysEqual,
+  assertEqual,
+  assertObjectsEqual,
+  countLetters,
+  eqArrays,
+  countOnly ,
+  eqObjects,
+  findKey,
+  findKeyByValue,
+  letterPositions,
+  map,
+  takeUntil,
+  without 
 
 };
