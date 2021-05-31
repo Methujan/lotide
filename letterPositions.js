de69bd3708e20cb033e658708fba8f32395f94ac
Methujan Jeyapalan <methujan_02@hotmail.com>
2021-05-31 20:17:20 +0000

    Refactored letterPositions

--- letterPositions.js
+++ letterPositions.js
@@ -1,42 +1,11 @@
-//eqArrays
-const eqArrays = function(array1, array2) {
-  let result = true;
-  
-  if (array1.length === array2.length) {
 
-    for (let i = 0; i < array1.length; i++) {
-      
-      if (array1[i] === array2[i]) {
-        result = true;
-
-      } else {
-        result = false;
-        return result;
-
-      }
-    }
-  } else {
-    result = false;
-    return result;
-  }
-
-  return result;
-};
-
-//assertArraysEqual
-const assertArraysEqual = function(array1, array2) {
-  if (eqArrays(array1, array2)) {
-    console.log(`✅✅✅  Assertion passed: ${array1} === ${array2}`);
-  } else {
-    console.log(`🛑🛑🛑  Assertion failed: ${array1} !== ${array2}`);
-  }
-};
 
 //letterPositions
 const letterPositions = function(sentence) {
   const results = {};
   for(letters of sentence){
     results[letters] = [];
+    //console.log(results[letters]);
   }
   for(let x in results){
   for(let i = 0; i < sentence.length; i++){
@@ -48,7 +17,9 @@ const letterPositions = function(sentence) {
 }
 return results;
 }
-console.log(letterPositions('hello'))
 
-assertArraysEqual(letterPositions("hello").e, [1]);
+module.exports = letterPositions;
 
+
+//assertArraysEqual(letterPositions("hello").e, [1]);
+//
